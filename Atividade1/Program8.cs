08a7cc4a79f4a01fc972be370df23041bd105794
Alan Torres De Sá <115950460+desaats@users.noreply.github.com>
2023-03-10 20:48:32 -0300

    Atividade8

---
+++ Atividade1/Program8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Declaração de variáveis
+        double construcao, casasU,salarioM, dolar,casasC;
+
+        //Entrada de dados 
+        Console.WriteLine("Escreva o valor do salário mínimo em reais:");
+        salarioM = int.Parse(Console.ReadLine());
+
+        //Processamento dos dados
+        dolar = salarioM / 5.15;
+        casasU= dolar * 150;
+        construcao = 10000000;
+        casasC = construcao / casasU;
+
+        //Saída da informação 
+        Console.WriteLine("A quantiade de casas que podemos construir são: " + casasC);
+    }
+}
